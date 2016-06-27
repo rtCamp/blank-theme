d9a1fde39a59252980201f33c36e2e8fa60affca
manish.songirkar <manish.songirkar@rtcamp.com>
2016-06-27 17:48:19 +0530

    Fixed broken markup

--- footer.php
+++ footer.php
@@ -11,16 +11,16 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer row" role="contentinfo">
+	<footer id="colophon" class="site-footer" role="contentinfo">
 		<?php if ( is_active_sidebar( 'sidebar-2' ) ){ ?>
-		<div class="rtp-footer-widgets-left large-12 column">
+		<div class="rtp-footer-widgets-left row">
 			<?php dynamic_sidebar( 'sidebar-2' ); ?>
 		</div>
 		<?php } ?>
-		<div class="site-info large-12 column">
+		<div class="site-info row-container">
 			<span class="blank-theme-copyright-text"><?php echo blank_theme_copyright_text(); ?></span>
 			<span class="sep"> | </span>
-			<a class="blank-theme-author-footer" href="<?php echo esc_url( 'http://underscore-me.com/' ); ?>" target="_blank" >Blank Theme</a>
+			<a class="blank-theme-author-footer" href="<?php echo esc_url( 'https://rtcamp.com/' ); ?>" target="_blank"><?php esc_html_e( 'Blank Theme', 'blank-theme' ); ?></a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
