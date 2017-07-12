c9a4bf8eb2353c713dc9ea9634e0fcf0d520d5ae
Bharat Parsiya <bnap00@gmail.com>
2017-07-12 20:33:16 +0530

    fix phpcs on comments page.

--- comments.php
+++ comments.php
@@ -28,8 +28,27 @@ if ( have_comments() ) :
 	?>
 		<h2 class="comments-title">
 			<?php
-			// translators: Number of comments.
-			echo sprintf( esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', number_format_i18n( get_comments_number() ), 'comments title', 'blank-theme' ) ) , esc_html( get_comments_number() ) , '<span>' . esc_html( get_the_title() ) . '</span>' );
+			$comments_number = get_comments_number();
+			if ( 1 === $comments_number ) {
+				printf(
+					/* translators: %s: post title */
+					esc_html_x( 'One thought on &ldquo;%s&rdquo;', 'comments title', '_s' ),
+					'<span>' . get_the_title() . '</span>'
+				);
+			} else {
+				printf( // WPCS: XSS OK.
+					/* translators: 1: number of comments, 2: post title */
+					esc_html( _nx(
+						'%1$s thought on &ldquo;%2$s&rdquo;',
+						'%1$s thoughts on &ldquo;%2$s&rdquo;',
+						$comments_number,
+						'comments title',
+						'_s'
+					) ),
+					number_format_i18n( $comments_number ),
+					'<span>' . get_the_title() . '</span>'
+				);
+			}
 			?>
 		</h2>
 
