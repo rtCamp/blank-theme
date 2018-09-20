efd438336eeb8b70713c3996b2b98f54cf7c9a1d
Manish Songirkar <manish.songirkar@rtcamp.com>
2018-09-20 10:42:31 +0530

    Add cache busting for static assets

--- inc/classes/class-assets.php
+++ inc/classes/class-assets.php
@@ -10,15 +10,44 @@ namespace Blank_Theme;
 class Assets extends Base {
 
 	/**
+	 * Retrive asset path.
+	 *
+	 * @param string $filename File name of which hash path retrive.
+	 *
+	 * @link https://danielshaw.co.nz/wordpress-cache-busting-json-hash-map/
+	 *
+	 * @return string
+	 */
+	public static function asset_path( $filename ) {
+
+		ob_start();
+		require_once get_template_directory() . '/build/manifest.json';
+		$map = ob_get_clean();
+
+		static $hash = null;
+
+		if ( null === $hash ) {
+			$hash = ( $map ) ? json_decode( $map, true ) : [];
+		}
+
+		if ( array_key_exists( $filename, $hash ) ) {
+			return BLANK_THEME_BUILD_URI . '/' . $hash[ $filename ];
+		}
+
+		return BLANK_THEME_BUILD_URI . '/' . $filename;
+
+	}
+
+	/**
 	 * Register scripts.
 	 *
 	 * @action wp_enqueue_scripts
 	 */
 	public function register_scripts() {
 
-		wp_register_script( 'blank-theme-main', BLANK_THEME_BUILD_URI . '/js/main.js', array( 'jquery' ), BLANK_THEME_VERSION, true );
-		wp_register_script( 'blank-theme-home', BLANK_THEME_BUILD_URI . '/js/home.js', array( 'jquery' ), BLANK_THEME_VERSION, true );
-		wp_register_script( 'blank-theme-single', BLANK_THEME_BUILD_URI . '/js/single.js', array( 'jquery' ), BLANK_THEME_VERSION, true );
+		wp_register_script( 'blank-theme-main', self::asset_path( 'main.js' ), array( 'jquery' ), BLANK_THEME_VERSION, true );
+		wp_register_script( 'blank-theme-home', self::asset_path( 'home.js' ), array( 'jquery' ), BLANK_THEME_VERSION, true );
+		wp_register_script( 'blank-theme-single', self::asset_path( 'single.js' ), array( 'jquery' ), BLANK_THEME_VERSION, true );
 
 		wp_enqueue_script( 'blank-theme-main' );
 
@@ -33,6 +62,7 @@ class Assets extends Base {
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
+
 	}
 
 	/**
@@ -42,9 +72,9 @@ class Assets extends Base {
 	 */
 	public function register_styles() {
 
-		wp_register_style( 'blank-theme-main', BLANK_THEME_BUILD_URI . '/css/main.css', array(), BLANK_THEME_VERSION );
-		wp_register_style( 'blank-theme-home', BLANK_THEME_BUILD_URI . '/css/home.css', array( 'blank-theme-main' ), BLANK_THEME_VERSION );
-		wp_register_style( 'blank-theme-single', BLANK_THEME_BUILD_URI . '/css/single.css', array( 'blank-theme-main' ), BLANK_THEME_VERSION );
+		wp_register_style( 'blank-theme-main', self::asset_path( 'main.css' ), array(), BLANK_THEME_VERSION );
+		wp_register_style( 'blank-theme-home', self::asset_path( 'home.css' ), array( 'blank-theme-main' ), BLANK_THEME_VERSION );
+		wp_register_style( 'blank-theme-single', self::asset_path( 'single.css' ), array( 'blank-theme-main' ), BLANK_THEME_VERSION );
 
 		wp_enqueue_style( 'blank-theme-main' );
 
